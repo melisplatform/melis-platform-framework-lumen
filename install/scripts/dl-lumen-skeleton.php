1d2cbcfee25a98875924f2bf8491eeaa38e7ce1b
jerremeirago <jrago@melistechnology.com>
2020-09-16 04:51:53 +0000

    make lumen storage writable

--- install/scripts/dl-lumen-skeleton.php
+++ install/scripts/dl-lumen-skeleton.php
@@ -4,10 +4,18 @@
 $isCliReqs = php_sapi_name() == 'cli' ? true : false;
 //third party Lumen
 $thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Lumen' : 'thirdparty/Lumen';
-
+    
+// lumen storage dir
 if (!is_dir($thirdPartyFolder)) {
-    return MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('lumen');
-}else{
+    // download lumen skeleton from marketplace
+    $message = MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('lumen');
+    // make sure storage dir is writable
+    chmod(getcwd() . '/thirdparty/Lumen/storage/', 0777);
+
+    return $message;
+
+} else {
+
     return [
         'success' => true,
         'message' => 'Lumen skeleton downloaded successfully'
